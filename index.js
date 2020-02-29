f87f964bd6d4b5e35f8ebd2efc5743019c5382dd
Raquel Dionísio <dionisio.raquel@gmail.com>
2020-02-29 13:44:52 +0100

    tried new option for this exercise to be added to sol2

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
-// PRINT EXPONENTIAL VALUES
+// PRINT EXPONENTIAL VALUES - FIRST SOLUTION
 // Write a function that accepts two numbers and validate that they are numbers.
 // After that, the function should print y exponential values starting from x.
 // For example if we have function(3, 5) The function should print 3 9 27 81 243.
 //Prints 5 exponential values of 3.
 // function(2, 8) The function prints 2 4 8 16 32 64 128 256. Prints 8 exponential values of 2.
 
-//HERE COMES THE SECOND SOLUTION
+function expValues(num1, num2) {
+  let valArr = []; // creates an empty array
+  let exp = 1; // creates a variable to store my exponential values
+  let result = 0; // creates an empty string for what is going to be printed
+  for (let i = 0; i < num2; i++) {
+    //creates a loop to add an exponential value of the first number until the index is equal to the second number
+    // if (!isNaN(num1) && !isNaN(num2)) {
+    //this option converts numbers passed as strings into numbers
+    if (
+      typeof parseInt(num1) === "number" &&
+      typeof parseInt(num2) === "number"
+    ) {
+      // this option NOT do that, so we'd need to use parseInt in addition - and then it WORKS
+      //creates the condition for the function
+      exp *= num1; // multiplies the value of exp with num1 until the condition in the for loop stops being true
+      valArr.push(exp); // pushes the values to a new array
+      result = valArr; // stores the new array in an empty variable
+    }
+  }
+  if (isNaN(num1) && isNaN(num2)) {
+    // checks if the data inserted by the user corresponds to numbers
+    result = `These are NOT numbers`;
+  }
+  return result;
+}
+
+console.log(expValues(2, 5));
+console.log(expValues("2", "5"));
+console.log(expValues("i", "t"));
